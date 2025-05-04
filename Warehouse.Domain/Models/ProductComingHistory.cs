947417b4f7e6642e932250cefcf95dd43130c32f
erik <zu2017@yandex.ru>
2025-05-04 22:14:40 +0500

    .

---
+++ Warehouse.Domain/Models/ProductComingHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Domain.Models
+{
+    public class ProductComingHistory
+    {
+        public Guid Id { get; }
+        public Customer CustomerId { get; }
+        public Product ProductId { get; }
+        public DateTime Date { get; }
+        public decimal TotalPrice { get; }  
+    }
+}
---
+++ Warehouse.Domain/Models/ProductLeavingHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Domain.Models
+{
+    public class ProductLeavingHistory
+    {
+        public Guid Id { get;  }
+        public Supplier SupplierId { get; }
+        public Product ProductId { get; }
+        public DateTime Date { get; }
+        public decimal TotalPrice { get; }
+    }
+}
